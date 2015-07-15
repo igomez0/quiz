ea10e1964de113af1a82c9729e58906b7d4fb7db
igomez0 <inigogomez1@gmail.com>
2015-07-15 15:54:30 +0200

    Revert "Despliegue DB en Heroku"
    This reverts commit bf0febe6e91f626cf6d73a62e7c8aa45aae3a261.

--- models/quiz.js
+++
@@ -1,7 +0,0 @@
-// Definicion del modelo de Quiz
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Quiz',
-            { pregunta:  DataTypes.STRING,
-              respuesta: DataTypes.STRING,
-            });
-}
