343aa8b4b4756e6871a3f803b850bd959aee1bfb
igomez0 <inigogomez1@gmail.com>
2015-07-18 11:59:34 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,21 @@
+var models = require('../models/models.js');
 
-// get /quizes/question
-
-exports.question=function(req,res) {
-res.render('quizes/question',{pregunta: 'Capital de Italia'});
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
 };
 
-
-
-// get /quizes/answer
-exports.answer=function(req,res) {
-		if (req.query.respuesta==='Roma') {
-			res.render('quizes/answer',{respuesta: 'Correcto'});
-			}else{
-			res.render('quizes/answer',{respuesta: 'Incorrecto'});	
-			}
-	};
-	
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+    }
+  })
+};
 
 
